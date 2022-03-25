5549b1bb6b45a4f79fce88f85b23406e38aa1e29
Christian Dean <christiandeandemesa@gmail.com>
2022-03-25 10:55:49 -0700

    frontend routes and added concurrently

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+// Imports BrowserRouter, Routes, and Route from react-router-dom.
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// Imports the Header component.
+import Header from './components/Header';
+// Imports the Dashboard component.
+import Dashboard from './pages/Dashboard';
+// Imports the Login component.
+import Login from './pages/Login';
+// Imports the Register component.
+import Register from './pages/Register';
+
+function App() {
+  return (
+    // BrowserRouter connects the browser's url with the React app.
+    // BrowserRouter must be wrapped around the entire app.
+    <BrowserRouter>
+      <div>
+        {/* Header component. */}
+        <Header/>
+        {/* Routes holds all of the Route paths. */}
+        <Routes>
+          {/* Each Route has a path which is the url it appends (e.g. path='/' goes to http://localhost:3000/). 
+              Each Route also has an element which is the component rendered at the url. */}
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+// Exports this component, so it can be imported elsewhere.
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+// Imports Link from react-router-dom.
+import {Link} from 'react-router-dom';
+// import react-icons
+
+function Header() {
+    return (
+        <header>
+            <div>
+                {/* Link works similar to <a href> by creating a hyperlink on the given text (e.g. Dashboard) to the url it appends. */}
+                <Link to='/'>Dashboard</Link>
+                {/* react-icon */}
+            </div>
+            <div>
+                <Link to='/login'>Login</Link>
+                {/* react-icon */}
+            </div>
+            <div>
+                <Link to='/register'>Register</Link>
+                {/* react-icon */}
+            </div>
+        </header>
+    );
+}
+
+export default Header;
